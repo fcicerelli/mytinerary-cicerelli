e23aacc217b13ba2d69669665fe6cfc1950804db
fcicerelli <fcicerellitic@gmail.com>
2023-08-02 09:38:22 -0300

    instalacion de tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
+  const nombre1 = 'My Tinerary'
+  const nombre2 = 'My'
+  const nombre3 = 'Tinerary'
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-full min-h-screen bg-red-500 flex flex-col">
+      <header className="h-[87px] bg-red-100 flex justify-center items-center">esto va a ser la barra de navegación</header>
+      <main className="grow">{nombre1}</main>
+      {console.log(nombre2)}
+      { /* Esto es un comentario */ }
+      <footer className="h-[49px] bg-pink-200">{`Este es el footer de ${nombre2} ${nombre3}`}</footer>
+    </div>
   )
 }
 
